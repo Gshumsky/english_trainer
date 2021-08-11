8c8374ad31a9675e11c10ca95f4c506531b9f7b1
Gshumsky <shumskygrigory@gmail.com>
2021-08-12 00:40:35 +0300

    added mistakes training

--- trainer.py
+++ trainer.py
@@ -1,8 +1,22 @@
 import json
 import random
 
-with open('/Users/grigoryshumsky/PycharmProjects/english_trainer/resourses/all_prep.json') as source:
+all_preps = '/Users/grigoryshumsky/PycharmProjects/english_trainer/resourses/all_prep.json'
+wrong_answers = '/Users/grigoryshumsky/PycharmProjects/english_trainer/resourses/wrong_answers.json'
+
+file = all_preps
+recording = False
+recorded_dict = {}
+
+if input("Train mistakes? y/n: ") == 'y':
+    file = wrong_answers
+else:
+    if input("Enable recording? y/n: ") == 'y':
+        recording = True
+
+with open(file) as source:
     parsed_source = json.load(source)
+
 for string in random.sample(sorted(parsed_source), len(parsed_source)):
     print(string)
     if input('enter preposition: ') == parsed_source[string]:
@@ -11,3 +25,7 @@ for string in random.sample(sorted(parsed_source), len(parsed_source)):
     else:
         print(f'Unfortunately, incorrect.The correct prep is "{parsed_source[string]}"')
         input()
+        recorded_dict[string] = parsed_source[string]
+if recording:
+    with open(wrong_answers, 'w') as recorded_json:
+        json.dump(recorded_dict, recorded_json)
